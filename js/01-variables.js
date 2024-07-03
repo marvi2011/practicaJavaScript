f18618a94f50fc060fcc52c5d1ea75e7c1bd2698
marvi2011 <marvi661@hotmail.com>
2024-07-03 12:37:55 -0300

    variable let

---
+++ js/01-variables.js
@@ -0,0 +1,16 @@
+// comentario de una linea 
+/* comentario 
+de 
+varias
+ lineas*/
+ //declarar o crear una variable (var - let - const)
+let edad = "30"
+let encendido = false
+//mostrar el contenido de una variable
+console.log(edad)
+document.write(edad)
+document.write("<p class='texto-resaltado'>Hola mundo</p>")
+document.write("Edad: " + edad + "<br>")
+//modificar el contenido de una variable
+edad = 30 + 3 
+document.write("Edad: " + edad)
