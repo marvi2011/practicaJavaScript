889cc4a042e4e8ea681bb7f534b72652865af5be
marvi2011 <marvi661@hotmail.com>
2024-07-03 13:21:46 -0300

    comenzamos a crear el programa para saber si alguien es mayor de edad ingresando la edad

---
+++ js/02-condicionales.js
@@ -0,0 +1,11 @@
+/*if (condicion logica){
+todas las lineas de codigo si se cumple la condicion logica
+}
+*/
+
+//crear un programa que le solicite al usuario ingresar su edad y saber si es mayor de edad o no//
+const edad = parseInt(prompt("Ingrese su edad: "))
+console.log(edad)
+//NaN is not a number
+//si la edad es mayor o igual a 18 sos mayor de edad
+
