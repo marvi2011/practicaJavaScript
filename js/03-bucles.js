13053fce94d0dd42ced4f4507ce9ee247294c341
marvi2011 <marvi661@hotmail.com>
2024-07-10 11:52:19 -0300

    comenzamos con bucles

---
+++ js/03-bucles.js
@@ -0,0 +1,10 @@
+//bucle while, do-while, for
+//while(condicion logica){
+//todas las lineas de codigo que quiero repetir}
+//agregar una línea que haga que la condicion logica se deje de complir}
+
+let numero =1;
+while(numero<=10){document.write(`<p> Línea ${numero} 1<p>`)
+    numero=numero+1;
+
+}
