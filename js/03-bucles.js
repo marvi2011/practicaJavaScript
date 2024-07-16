30b677c00171bcab498f0347c3ea9ac98f42ee93
Victoria Gramajo <marvi661@hotmail.com>
2024-07-15 21:59:24 -0300

    agregamos unas estructura de variable for

--- js/03-bucles.js
+++ js/03-bucles.js
@@ -1,13 +1,26 @@
 // bucles while, do-while, for
 
 // while(condicion logica){
-    // todas las lineas de codigo que quiero repetir
-    // agregar una linea que haga que la condicion logica se deje de cumplir
+// todas las lineas de codigo que quiero repetir
+// agregar una linea que haga que la condicion logica se deje de cumplir
 // }
 
+/*let numero = 1;
+while (numero <= 10) {
+  document.write(`<p>Linea número ${numero}</p>`);
+  numero = numero + 1;
+}*/
 
-let numero = 1;
-while(numero<=10){
-    document.write(`<p>Linea número ${numero}</p>`)
-    numero =  numero + 1;
+/* do{Todas las lineas de codigo que quiero repetir
+agregar una lina que haga que la condicion logica se deje de cumplir
+} while(condicion logica)*/
+let renglon = 1;
+do {
+  document.write("<p>Renglón número </p>" + renglon);
+  renglon++; // se puede simplificar asi = renglon + 1;
+} while (renglon <= 5);
+
+/* for(inicializar euna variable; condicion logica; incrementar/decrementar la variable){todas las lineas de codigo que quiero repetir}*/
+for (let contador = 10; contador >= 1; contador--) {
+  document.write("<p>Cuenta regresiva N°" + contador +" </p>");
 }
