0c57daf6941da4834d2c2c1430ac497da26b8538
Victoria Gramajo <marvi661@hotmail.com>
2024-07-24 00:27:58 -0300

    agregamos find, includes, filter, map, forEach

--- js/05-arrays.js
+++ js/05-arrays.js
@@ -13,7 +13,7 @@ function mostrarSeries() {
 const peliculas = [];
 
 //como declara un array con datos
-const series = ["friends", 4, "the big bang theory", true, "the nanny"];
+const series = ["friends", "4", "the big bang theory", true, "the nanny"];
 mostrarSeries();
 
 //cantidad de elementos de un array
@@ -36,11 +36,11 @@ document.write("</ul>");*/
 mostrarSeries();
 
 //agregar un elemento en el array
-series.unshift("vikings", 2024); //al comienzo
+series.unshift("vikings", "2024"); //al comienzo
 mostrarSeries();
 
 //agregar elementos al final del array
-series.push("breaking bad", 300);
+series.push("breaking bad", "300");
 mostrarSeries();
 
 //agregar elementos en el medio del array
@@ -60,5 +60,71 @@ series.splice(5, 1);
 mostrarSeries();
 //series.splice(5) borra todos los elementos desde la posicion 5 en adelante
 
-series[3]="pokemon"
-mostrarSeries()
+series[3] = "pokemon";
+mostrarSeries();
+
+series.push(
+  "Harry Potter y la piedra filosofal",
+  "Rambo I",
+  "Rambo II",
+  "Harry Potter y la cámara secreta",
+  "Rambo III",
+  "Harry Potter y el prisionero de azkaban"
+);
+mostrarSeries();
+
+//buscar un elemento en el array
+const harryPotter = series.find(
+  (item) => item === "Harry Potter y la cámara secreta"
+);
+const vengadores = series.find((item) => item === "vengadores");
+const harryP = series.find((item) => item === "Harry Potter");
+const harryPotter2 = series.find((item) =>
+  item.toLowerCase().includes("harry potter")
+);
+
+console.log(series[0].toLowerCase);
+console.log(harryPotter);
+console.log(vengadores);
+console.log(harryP);
+document.write(`<h3>La peli buscada: ${harryPotter}</h3>`);
+document.write(`<h3>La peli buscada: ${vengadores}</h3>`);
+//existe valor valido en la varialbel vengadores?
+if (vengadores) {
+  document.write(`<h3>La peli buscada: Encpntre la peli</h3>`);
+} else {
+  document.write(`<h3>La peli buscada: No se encontró Vengadores</h3>`);
+}
+
+//operadpr ternario (condicion logica)? todo lo wue quiero si la condicion se cumplle : todo lo que quiero hacer si no se cumple la condicion
+
+vengadores
+  ? document.write(`<h3>La peli buscada: ${vengadores}</h3>`)
+  : document.write(`<h3>La peli buscada: No se encontró Vengadores</h3>`);
+
+document.write(
+  `<h3>Peli buscada: ${
+    vengadores ? vengadores : "no se encontro vengadores"
+  }</h3>`
+);
+const peli = "Harry Potter y el misterio del principe";
+console.log(peli.includes("Harry Potter"));
+console.log(peli.includes("Voldemort"));
+
+//buscar toda la saga de rambo (filter)
+const sagaRambo = series.filter((elemento) => elemento === "Rambo");
+console.log(sagaRambo);
+const sagaRambo2 = series.filter((elemento) => elemento.includes("Rambo"));
+console.log(sagaRambo2);
+
+document.write(
+  `<h2>Array de Rambo, cantidad de elementos: ${sagaRambo2.length}</h2>`
+);
+document.write("<ol>");
+sagaRambo2.map((item, posicion) =>
+  document.write(`<li>${item} - posicion:${posicion}</li>`)
+);
+document.write("</ol>");
+
+//forEach
+series.forEach((item) => console.log("forEach -" + item));
